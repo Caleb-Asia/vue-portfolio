02a326d039f8a979d3c370ab4738603a2076e417
Caleb-Asia <calebasia1811@gmail.com>
2026-09-23 16:12:23 +0200

    Tryna deploy on netlify

--- src/router.js
+++ src/router.js
@@ -1,12 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
-// ... imports ...
+import HomeView from "./views/HomeView.vue";
+import ProfileView from "./views/ProfileView.vue";
+import PortfolioView from "./views/PortfolioView.vue";
+import SkillsView from "./views/SkillsView.vue";
+import ContactView from "./views/ContactView.vue";
+import HobbiesView from "./views/HobbiesView.vue";
 
 export default createRouter({
-  history: createWebHistory(), // <--- Back to standard history mode
+  history: createWebHistory(),
   routes: [
     { path: "/", name: "home", component: HomeView },
-    // ... your other routes ...
-    { path: "/:pathMatch(.*)*", redirect: "/" } // Keep this, it's good practice
+    { path: "/profile", name: "profile", component: ProfileView },
+    { path: "/portfolio", name: "portfolio", component: PortfolioView },
+    { path: "/skills", name: "skills", component: SkillsView },
+    { path: "/hobbies", name: "hobbies", component: HobbiesView },
+    { path: "/contact", name: "contact", component: ContactView },
+    { path: "/:pathMatch(.*)*", redirect: "/" }
   ],
   scrollBehavior: () => ({ top: 0 }),
 });
